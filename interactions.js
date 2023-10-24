67de0f91da241c8f769d5b2ce6d0bd8bb0ca5929
Conrado <42232126+bojorquezc@users.noreply.github.com>
2023-10-23 18:29:37 -0700

    Complete core layout

---
+++ interactions.js
@@ -0,0 +1,21 @@
+const navItem = {
+    burgerMenu: document.querySelector('.burger_menu'),
+    closeIcon: document.querySelector('.close_icon'),
+    sideBarMenuClosed: document.querySelector('.sidebar_menu_closed'),
+    sideBarMenuOpen: document.querySelector('.sidebar_menu_open'),
+}
+
+navItem.burgerMenu.addEventListener('click', expandSideBar);
+navItem.closeIcon.addEventListener('click', contractSideBar);
+
+function expandSideBar() {
+    navItem.sideBarMenuOpen.style.display = 'flex';
+    navItem.sideBarMenuClosed.style.display = 'none';
+    console.log('expand side bar')
+}
+
+function contractSideBar() {
+    navItem.sideBarMenuOpen.style.display = 'none';
+    navItem.sideBarMenuClosed.style.display = 'flex';
+    console.log('contract side bar')
+}
