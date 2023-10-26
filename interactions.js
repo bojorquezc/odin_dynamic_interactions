3dfc2b96127edd502f9db46c0b28f90f3171b030
Conrado <42232126+bojorquezc@users.noreply.github.com>
2023-10-26 14:48:27 -0700

    Mobile menu complete

--- interactions.js
+++ interactions.js
@@ -1,31 +1,69 @@
-const expandMenuItem = document.querySelector('.expand_menu');
-expandMenuItem.addEventListener('click', expandAndContractMenu);
-
-function expandAndContractMenu() {
-    const menuItem = document.querySelectorAll('.expand_menu_item');
-    if (expandMenuItem.textContent === 'Expand Menu') {
-        expandMenuItem.textContent = 'Contract Menu';
-        menuItem.forEach((item) => {
+// General functions to expand / contract / style
+function expandAndContractMenu(trigger, menuItems) {
+    if (trigger.textContent === 'Expand Menu') {
+        trigger.textContent = 'Contract Menu';
+        menuItems.forEach((item) => {
             item.style.display = 'block';
         })
     } else {
-        expandMenuItem.textContent = 'Expand Menu';
-        menuItem.forEach((item) => {
+        trigger.textContent = 'Expand Menu';
+        menuItems.forEach((item) => {
             item.style.display = 'none';
         })
     }
-    updateOddBackground();
 }
 
-function updateOddBackground() {
-    const menuItem = document.querySelectorAll('.expand_menu_item');
-    const array = Array.from(menuItem);
+function updateOddBackground(nodeListToStyle) {
+    const array = Array.from(nodeListToStyle);
     array.forEach((item) => {
         if (array.indexOf(item) % 2 === 0) {
             item.style.backgroundColor = '#fc9d00';
-            console.log(array.indexOf(item));
         }
     })
 }
 
-// test commit
+// Desktop navigation interactions
+const desktopMenu = {
+    trigger: document.querySelector('.expand_menu'),
+    items: document.querySelectorAll('.expand_menu_item'),
+}
+desktopMenu.trigger.addEventListener('click', expandContractDesktop);
+
+function expandContractDesktop() {
+    expandAndContractMenu(desktopMenu.trigger, desktopMenu.items);
+    updateOddBackground(desktopMenu.items);
+}
+
+// Mobile navigation interactions
+const mobileMenu = {
+    burgerIcon: document.querySelector('.burger_icon'),
+    trigger: document.querySelector('.expand_menu_mobile'),
+    items: document.querySelectorAll('.expand_menu_item_mobile'),
+    mainNavigation: document.querySelector('.main_navigation_mobile'),
+}
+mobileMenu.burgerIcon.addEventListener('click', displayMobileMenu);
+mobileMenu.trigger.addEventListener('click', expandContractMobile);
+mobileMenu.mainNavigation.style.display = 'none';
+
+function displayMobileMenu () {
+    if (mobileMenu.mainNavigation.style.display === 'none') {
+        console.log('Houston this is a test');
+        mobileMenu.mainNavigation.style.display = 'block';
+    } else {
+        mobileMenu.mainNavigation.style.display = 'none';
+    }
+}
+
+function expandContractMobile() {
+    expandAndContractMenu(mobileMenu.trigger, mobileMenu.items);
+    updateOddBackground(mobileMenu.items);
+    mobileMenuSpacing();
+}
+
+function mobileMenuSpacing() {
+    if (mobileMenu.trigger.textContent === 'Contract Menu') {
+        mobileMenu.trigger.style.paddingBottom = '2rem';
+    } else {
+        mobileMenu.trigger.style.paddingBottom = '0rem'; 
+    }
+}
