a981d79da73c6ccc7fb39dcb82a466c4707c5983
Conrado <42232126+bojorquezc@users.noreply.github.com>
2023-10-28 21:47:00 -0700

    Display image in slideshow area

--- gallery.js
+++ gallery.js
@@ -6,14 +6,28 @@ slideShow = [
     './images/chess_news_4.png'
 ]
 
-showImageInCarrousel = () => {
-    const slideShowCarrousel = document.querySelector('.slideshow_carrousel');
+const slideShowSection = {
+    carrousel: document.querySelector('.slideshow_carrousel'),
+    display: document.querySelector('.slideshow_display'),
+}
 
+showImageInSlideshow = (e) => {
+    const index = e.target.dataset.arrayIndex;
+    if (slideShow[index] !== undefined) {
+        slideShowSection.display.replaceChildren();
+        const slideShowImage = document.createElement('img');
+        slideShowImage.src = slideShow[index];
+        slideShowSection.display.append(slideShowImage);
+    }
+}
+
+showImageInCarrousel = () => {
     for (let image in slideShow) {
         const carrouselImage = document.createElement('img');
         carrouselImage.src = slideShow[image];
-        carrouselImage.addEventListener('click', console.log('image clicked'));
-        slideShowCarrousel.appendChild(carrouselImage)
+        carrouselImage.dataset.arrayIndex = image;
+        carrouselImage.addEventListener('click', showImageInSlideshow);
+        slideShowSection.carrousel.appendChild(carrouselImage);
     }
 }
 
